2f5163827d71aba6fea4c41df288d9a7bbf4dcdc
Ganesh Kotagiri <123595702+Ganeshkotagiri-12@users.noreply.github.com>
2024-03-19 01:36:35 -0400

    intial main for email generator via Spring

--- src/main/java/EmailApp.java
+++ src/main/java/EmailApp.java
@@ -1,13 +1,9 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.Arrays;
 
 public class EmailApp {
     public static void main(String[]args){
         try(var context=new AnnotationConfigApplicationContext(EmailConfig.class)){
-            //Arrays.stream( context.getBeanDefinitionNames()).forEach(System.out::println   );
             System.out.println(context.getBean(EmailConfig.class));
         }
     }
